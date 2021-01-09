76d52eda4945e11f6da2355713ef2b6f87d0ecf2
Aleksejs Sinicins <aleksejs@infogr.am>
2021-01-09 10:23:48 +0200

    built version

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,5 @@
+import type { ReactNativeHealthkit } from './types';
+declare const Healthkit: ReactNativeHealthkit;
+export * from './types';
+export * from './native-types';
+export default Healthkit;
